a63b820b03289a0f61cafcebc35eb3dba978ce9e
shokohsc <shokohsc@gmail.com>
2026-04-23 14:31:11 +0200

    chore(02-hardware): updated terraform backends

--- 02-hardware/mikrotik/backend.tf
+++ 02-hardware/mikrotik/backend.tf
@@ -1,5 +1,7 @@
 terraform {
-  backend "local" {
-    path = var.terraform_state_path
+  backend "http" {
+    address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
+    lock_address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
+    unlock_address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
   }
 }
