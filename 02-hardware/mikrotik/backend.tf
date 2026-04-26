e3418edc4bd47f49705aacd677258cd71ff46c94
shokohsc <shokohsc@gmail.com>
2026-04-26 20:48:55 +0200

    chore(02-hardware): postgres terraform backend

--- 02-hardware/mikrotik/backend.tf
+++ 02-hardware/mikrotik/backend.tf
@@ -1,7 +1,13 @@
+# terraform {
+#   backend "http" {
+#     address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
+#     lock_address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
+#     unlock_address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
+#   }
+# }
+
 terraform {
-  backend "http" {
-    address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
-    lock_address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
-    unlock_address = "http://localhost:6061/?type=git&repository=https://github.com/shokohsc/tfstate&ref=main&state=02-hardware/mikrotik/state.json"
+  backend "pg" {
+    conn_str = "postgres://postgres:opentofu@localhost:5432/opentofu?sslmode=disable"
   }
 }
