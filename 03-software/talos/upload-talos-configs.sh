4ab178ac1067ee1bff92de4712cc816824d47723
shokohsc <contact@shokohsc.com>
2026-05-28 13:42:29 +0000

    chore(03-software/talos): add talos config upload script

---
+++ 03-software/talos/upload-talos-configs.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Upload talos configuration files to booter & remote-config node.
+# Usage: ./upload-talos-configs.sh
+
+set -euo pipefail
+
+TALOSCONFIGS_HOST="shokohsc@tracer.home.arpa"
+REMOTE_CONFIG_PATH="/home/shokohsc/talos-pxe/config"
+
+log() { echo "[$(date +'%Y-%m-%d %H:%M:%S')] $*"; }
+
+log "Uploading talos configuration files to $TALOSCONFIGS_HOST"
+rsync -avz --delete --progress ./clusterconfig/talos-sombra.yaml "$TALOSCONFIGS_HOST:$REMOTE_CONFIG_PATH/m/94-C6-91-A2-82-AD.yaml"
+rsync -avz --delete --progress ./clusterconfig/talos-lucio.yaml "$TALOSCONFIGS_HOST:$REMOTE_CONFIG_PATH/m/1C-69-7A-04-0B-76.yaml"
+rsync -avz --delete --progress ./clusterconfig/talos-zarya.yaml "$TALOSCONFIGS_HOST:$REMOTE_CONFIG_PATH/m/94-C6-91-1C-FF-2E.yaml"
+rsync -avz --delete --progress ./clusterconfig/talos-mercy.yaml "$TALOSCONFIGS_HOST:$REMOTE_CONFIG_PATH/m/1C-69-7A-69-D9-1E.yaml"
+rsync -avz --delete --progress ./clusterconfig/talos-winston.yaml "$TALOSCONFIGS_HOST:$REMOTE_CONFIG_PATH/m/70-85-C2-5E-D0-D3.yaml"
+
+rsync -avz --delete --progress ./clusterconfig/talos-worker-vm.yaml "$TALOSCONFIGS_HOST:$REMOTE_CONFIG_PATH/p/worker-vm.yaml"
+rsync -avz --delete --progress ./clusterconfig/talos-worker-vm-gpu.yaml "$TALOSCONFIGS_HOST:$REMOTE_CONFIG_PATH/p/worker-vm-gpu.yaml"
