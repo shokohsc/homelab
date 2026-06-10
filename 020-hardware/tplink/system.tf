33448d980ffc3c260abd28dacc64792fd42cc4ae
shokohsc <shokohsc@gmail.com>
2026-06-10 23:17:59 +0200

    wip

--- 020-hardware/tplink/system.tf
+++ 020-hardware/tplink/system.tf
@@ -4,4 +4,9 @@ resource "openwrt_system" "system" {
   ttylogin     = "0"
   log_size     = "64"
   urandom_seed = "0"
+  log_ip       = cidrhost(local.vlan_cidrs["60"], 3)
+  log_port     = "601"
+  log_proto    = "tcp"
+  conloglevel  = "8"
+  cronloglevel = "5"
 }
