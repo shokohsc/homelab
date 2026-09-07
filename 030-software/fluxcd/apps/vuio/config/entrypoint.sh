f5126e2353169f1b6503cd17faff761af1b498ed
shokohsc <shokohsc@gmail.com>
2026-09-07 18:58:17 +0200

    wip

--- 030-software/fluxcd/apps/vuio/config/entrypoint.sh
+++ 030-software/fluxcd/apps/vuio/config/entrypoint.sh
@@ -388,7 +388,7 @@ main() {
     log "Starting VuIO as user vuio with command: $*"
 
     # Switch to vuio user and execute the command
-    # exec su-exec vuio "$@"
+    sleep 600 && exit 0
     # /app/vuio "$@"
 }
 
