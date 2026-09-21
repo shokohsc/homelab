08296162334c2ece7155142a156ecb37c1f0de12
shokohsc <shokohsc@gmail.com>
2026-09-21 15:55:07 +0200

    wip

--- 030-software/fluxcd/apps/shell2http/config/restart-vpn-egress-gateway.sh
+++ 030-software/fluxcd/apps/shell2http/config/restart-vpn-egress-gateway.sh
@@ -14,44 +14,7 @@ if [ "${RESTART_COUNT}" -gt 8 ]; then
     exit 0
 fi
 
-# # Define the deployment name
-# DEPLOYMENT_NAME="vpn-egress-gateway"
-# NAMESPACE="vpn-egress-gateway"  # Optional: specify the namespace if not using the default
+kubectl rollout restart deployment/vpn-egress-gateway -n vpn-egress-gateway
 
-# # Get the pod names associated with the deployment
-# # POD_NAMES=$(kubectl get pods -l app.kubernetes.io/name=$DEPLOYMENT_NAME -n $NAMESPACE -o json | jq -r '.items[].metadata.name')
-# POD_NAME=$(kubectl get pods -l app.kubernetes.io/name=$DEPLOYMENT_NAME -n $NAMESPACE -o json | jq -r '.items[].metadata.name')
-
-# # # Print the pod names
-# # echo "Pods associated with deployment '$DEPLOYMENT_NAME':"
-# # echo "$POD_NAME"
-
-
-# # Variables
-# # POD_NAME="your-pod-name"
-# NAMESPACE="vpn-egress-gateway"
-# CONTAINER_NAME="openvpn"
-
-# # Check if the pod is running
-# POD_STATUS=$(kubectl get pod $POD_NAME -n $NAMESPACE -o json | jq -r '.status.phase')
-# if [ "$POD_STATUS" != "Running" ]; then
-#   echo "Pod $POD_NAME is not running. Current status: $POD_STATUS"
-#   exit 1
-# fi
-
-# # Check if the container is running
-# CONTAINER_STATUS=$(kubectl get pod $POD_NAME -n $NAMESPACE -o json | jq -r '.status.containerStatuses[] | select(.name == "'$CONTAINER_NAME'") | .state.running')
-# if [ -z "$CONTAINER_STATUS" ] || [ "$CONTAINER_STATUS" == "null" ]; then
-#   echo "Container $CONTAINER_NAME is not running."
-#   exit 1
-# fi
-
-# # Execute a command in the container
-# kubectl exec -it $POD_NAME -n $NAMESPACE -c $CONTAINER_NAME -- /bin/bash -c "your-command-here"
-
-
-PID=$(kubectl exec deployment/vpn-egress-gateway -n vpn-egress-gateway -c openvpn -- ps aux |grep /usr/bin/get_ovpn_profile.sh |awk '{print $1}' | head -n 1)
-kubectl exec deployment/vpn-egress-gateway -n vpn-egress-gateway -c openvpn -- kill -TERM "${PID}"
-
-log "Killed process: ${PID}, exiting..."
+log "Restart deployment vpn-egress-gateway"
 exit 0
